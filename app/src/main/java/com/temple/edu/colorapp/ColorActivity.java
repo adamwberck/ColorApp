ba3731a9dbf51b96aed51110528808f5e2ba703d
Adam <adamwberck@gmail.com>
2020-02-17 00:47:56 -0500

    Colors added have to fix margins

--- app/src/main/java/com/temple/edu/colorapp/ColorActivity.java
+++ app/src/main/java/com/temple/edu/colorapp/ColorActivity.java
@@ -2,7 +2,12 @@ package com.temple.edu.colorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+        Spinner colorSpinner = findViewById(R.id.spinner_color);
+        colorSpinner.setAdapter(new ColorAdapter(this));
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String name = ((TextView)view.findViewById(R.id.text_color)).getText().toString();
+                ColorActivity.this.findViewById(R.id.layout_color).setBackgroundColor(
+                        Color.parseColor(name));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
